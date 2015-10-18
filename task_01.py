b36a2025ec77dd162f5aa59620adcd141b795c37
rrafiringa <rrafiringa@gmail.com>
2015-10-18 20:50:34 +0000

    task_01.py: Replaced "substraction" with "subtraction"

--- task_01.py
+++ task_01.py
@@ -8,5 +8,4 @@ ELEMENTS = (None, 'Hydrogen', 'Helium',
             'Boron', 'Carbon')
 
 OPERATIONS_ORDER = ['Parentheses', 'Exponents', 'Multiplication',
-              'Division', 'Addition', 'Substraction']
-
+              'Division', 'Addition', 'Subtraction']
